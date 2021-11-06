a7b7480e8fbb1d593b49434f038de654753224ef
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2021-11-06 16:22:10 +0530

    Update FastIO.java

--- FastIO.java
+++ FastIO.java
@@ -1,67 +1,73 @@
-// Working program with FastReader
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
+public class Main{
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str=br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+		private final BufferedWriter bw;
 
-		public FastReader()
-		{
-			br = new BufferedReader(
-				new InputStreamReader(System.in));
+		public FastWriter() {
+			this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		}
 
-		String next()
-		{
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				}
-				catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
+		public void print(Object object) throws IOException {
+			bw.append("" + object);
 		}
 
-		int nextInt() { return Integer.parseInt(next()); }
-
-		long nextLong() { return Long.parseLong(next()); }
-
-		double nextDouble()
-		{
-			return Double.parseDouble(next());
+		public void println(Object object) throws IOException {
+			print(object);
+			bw.append("\n");
 		}
 
-		String nextLine()
-		{
-			String str = "";
-			try {
-				str = br.readLine();
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
+		public void close() throws IOException {
+			bw.close();
 		}
 	}
-
-	public static void main(String[] args)
-	{
-		FastReader s = new FastReader();
-		int n = s.nextInt();
-		int k = s.nextInt();
-		int count = 0;
-		while (n-- > 0) {
-			int x = s.nextInt();
-			if (x % k == 0)
-				count++;
-		}
-		System.out.println(count);
-	}
+    public static void main(String[] args) {
+        try {
+            FastReader in=new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases=in.nextInt();
+            while(testCases-- > 0){
+                // write code here
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
 }
