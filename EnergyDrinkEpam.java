2a99c65811df0d71d82c398e6d184470bc80c216
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2021-11-07 19:49:17 +0530

    Create EnergyDrinkEpam.java

---
+++ EnergyDrinkEpam.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class EnergyDrink
+{
+    
+    static boolean isDescendingorequal(ArrayList<Integer> arr)
+    {
+        for(int i=0;i<arr.size()-1;i++)
+        {
+            if(arr.get(i)<arr.get(i+1))
+                return false;
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=Integer.parseInt(sc.nextLine());
+        int count=0;
+        ArrayList<Integer> energies=new ArrayList<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            energies.add(sc.nextInt());
+        }
+        
+        while(energies.size()>1 && !isDescendingorequal(energies))
+        {
+            int i=energies.size()-1;
+            
+            while(i>=1 && energies.size()>1)
+            {
+                if(energies.get(i)>energies.get(i-1))
+                {
+                    energies.remove(i);
+                }
+                i=i-1;
+            }
+            
+            count++;
+        }
+        
+        System.out.println(count);
+        
+        
+    }
+}
+
+
+
+
