e83c92da1e43cb33a50a78384c1443abd505765d
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2021-12-22 22:51:17 +0530

    Create kadanealgorithm.java

---
+++ kadanealgorithm.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int localmax=nums[0];
+        
+        int globalmax=nums[0];
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            localmax=Math.max(nums[i],localmax+nums[i]);
+            globalmax=Math.max(globalmax,localmax);
+        }
+        return globalmax;
+    }
+}
